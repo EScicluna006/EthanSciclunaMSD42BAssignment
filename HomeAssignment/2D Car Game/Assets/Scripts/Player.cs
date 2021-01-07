c91b145b199371ac962e8c00cf970b1c52fe341e
EScicluna006 <73096450+EScicluna006@users.noreply.github.com>
2021-01-07 18:39:47 +0100

    BAckground Scroller fixed and Player

---
+++ HomeAssignment/2D Car Game/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Move()
+    {
+        //var is used as a generic variable. VS allows us to use var and it will set its type depending on the value it will have
+        // deltaX will be updated with the input that will happen on the x-axis, left and right
+        var deltaX = Input.GetAxis(“Horizontal”);
+        var newXPos = transform.position.x + deltaX;
+
+        //the x-position will be updated according to the newXPos - whether left or right. Y position will be the same
+        transform.position = new Vector2(newXPos, transform.position.y);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+}
